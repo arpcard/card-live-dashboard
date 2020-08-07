80976f4047fa41e27514195fc6a436493dd8bd4f
Aaron Petkau <aaron.petkau@gmail.com>
2020-08-07 12:13:59 -0500

    Test for all cutoff

--- card_live_dashboard/test/integration/model/test_CardLiveData.py
+++ card_live_dashboard/test/integration/model/test_CardLiveData.py
@@ -9,14 +9,16 @@ TIME_FMT = '%Y-%m-%d %H:%M:%S'
 MAIN_DF = pd.DataFrame(
     columns=['filename', 'timestamp'],
     data=[['file1', '2020-08-05 16:27:32.996157'],
-          ['file2', '2020-08-06 16:27:32.996157']
-          ]
+          ['file2', '2020-08-06 16:27:32.996157'],
+          ['file3', '2020-08-07 16:27:32.996157'],
+          ],
 )
 
 OTHER_DF = pd.DataFrame(
     columns=['filename', 'timestamp'],
     data=[['file1', '2020-08-05 16:27:32.996157'],
-          ['file2', '2020-08-06 16:27:32.996157']
+          ['file2', '2020-08-06 16:27:32.996157'],
+          ['file3', '2020-08-07 16:27:32.996157'],
           ]
 ).set_index('filename')
 
@@ -24,7 +26,8 @@ RGI_DF = pd.DataFrame(
     columns=['filename', 'timestamp', 'rgi_main.Cut_Off', 'rgi_main.Drug Class'],
     data=[['file1', '2020-08-05 16:27:32.996157', 'Perfect', 'class1; class2'],
           ['file1', '2020-08-05 16:27:32.996157', 'Strict', 'class1; class2; class3'],
-          ['file2', '2020-08-06 16:27:32.996157', 'Perfect', 'class1; class4']
+          ['file2', '2020-08-06 16:27:32.996157', 'Perfect', 'class1; class4'],
+          ['file3', '2020-08-07 16:27:32.996157', None, None],
           ]
 ).set_index('filename')
 
@@ -40,17 +43,17 @@ DATA = CardLiveData(main_df=MAIN_DF,
 def test_select_by_time_keepall():
     data = DATA
     start = datetime.strptime('2020-08-05 00:00:00', TIME_FMT)
-    end = datetime.strptime('2020-08-07 00:00:00', TIME_FMT)
+    end = datetime.strptime('2020-08-08 00:00:00', TIME_FMT)
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select_by_time(start, end)
-    assert 2 == len(data), 'Invalid number after selection'
-    assert 2 == len(data.main_df), 'Invalid number after selection'
-    assert {'file1', 'file2'} == data.files(), 'Invalid files'
-    assert 3 == len(data.rgi_parser.df_rgi), 'Invalid number after selection'
-    assert 2 == len(data.rgi_kmer_df), 'Invalid number after selection'
-    assert 2 == len(data.lmat_df), 'Invalid number after selection'
-    assert 2 == len(data.mlst_df), 'Invalid number after selection'
+    assert 3 == len(data), 'Invalid number after selection'
+    assert 3 == len(data.main_df), 'Invalid number after selection'
+    assert {'file1', 'file2', 'file3'} == data.files(), 'Invalid files'
+    assert 4 == len(data.rgi_parser.df_rgi), 'Invalid number after selection'
+    assert 3 == len(data.rgi_kmer_df), 'Invalid number after selection'
+    assert 3 == len(data.lmat_df), 'Invalid number after selection'
+    assert 3 == len(data.mlst_df), 'Invalid number after selection'
 
 
 def test_select_by_time_keepone():
@@ -58,7 +61,7 @@ def test_select_by_time_keepone():
     start = datetime.strptime('2020-08-05 00:00:00', TIME_FMT)
     end = datetime.strptime('2020-08-06 00:00:00', TIME_FMT)
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select_by_time(start, end)
     assert 1 == len(data), 'Invalid number after selection'
     assert 1 == len(data.main_df), 'Invalid number after selection'
@@ -74,7 +77,7 @@ def test_select_by_time_keepnone():
     start = datetime.strptime('2020-08-05 00:00:00', TIME_FMT)
     end = datetime.strptime('2020-08-05 00:00:00', TIME_FMT)
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select_by_time(start, end)
     assert 0 == len(data), 'Invalid number after selection'
     assert 0 == len(data.main_df), 'Invalid number after selection'
@@ -89,7 +92,7 @@ def test_select_time_keepone():
     start = datetime.strptime('2020-08-05 00:00:00', TIME_FMT)
     end = datetime.strptime('2020-08-06 00:00:00', TIME_FMT)
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select(table='main', by='time', start=start, end=end)
     assert 1 == len(data), 'Invalid number after selection'
     assert 1 == len(data.main_df), 'Invalid number after selection'
@@ -104,7 +107,7 @@ def test_select_rgi_parser_keepone():
     data = DATA
     rgi_parser = RGIParser(RGI_DF.loc['file1'])
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select_from_rgi_parser(rgi_parser)
     assert 1 == len(data), 'Invalid number after selection'
     assert 1 == len(data.main_df), 'Invalid number after selection'
@@ -118,7 +121,7 @@ def test_select_rgi_parser_keepone():
 def test_select_rgi_cutoff_perfect():
     data = DATA
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select(table='rgi', by='cutoff', type='row', level='perfect')
     assert 2 == len(data), 'Invalid number after selection'
     assert 2 == len(data.main_df), 'Invalid number after selection'
@@ -134,7 +137,7 @@ def test_select_rgi_cutoff_perfect():
 def test_select_rgi_cutoff_strict():
     data = DATA
 
-    assert 2 == len(data), 'Data not initialized to correct number of entries'
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
     data = data.select(table='rgi', by='cutoff', type='row', level='strict')
     assert 1 == len(data), 'Invalid number after selection'
     assert 1 == len(data.main_df), 'Invalid number after selection'
@@ -145,3 +148,19 @@ def test_select_rgi_cutoff_strict():
     assert 1 == len(data.rgi_kmer_df), 'Invalid number after selection'
     assert 1 == len(data.lmat_df), 'Invalid number after selection'
     assert 1 == len(data.mlst_df), 'Invalid number after selection'
+
+
+def test_select_rgi_cutoff_all():
+    data = DATA
+
+    assert 3 == len(data), 'Data not initialized to correct number of entries'
+    data = data.select(table='rgi', by='cutoff', type='row', level='all')
+    assert 3 == len(data), 'Invalid number after selection'
+    assert 3 == len(data.main_df), 'Invalid number after selection'
+    assert {'file1', 'file2', 'file3'} == data.files(), 'Invalid files'
+    assert 4 == len(data.rgi_parser.df_rgi), 'Invalid number after selection'
+    assert {'Strict', 'Perfect', None} == set(data.rgi_parser.df_rgi['rgi_main.Cut_Off'].tolist()), 'Invalid cutoff values'
+    assert {'class1', 'class2', 'class3', 'class4'} == data.rgi_parser.all_drugs(), 'Invalid drug classes'
+    assert 3 == len(data.rgi_kmer_df), 'Invalid number after selection'
+    assert 3 == len(data.lmat_df), 'Invalid number after selection'
+    assert 3 == len(data.mlst_df), 'Invalid number after selection'
