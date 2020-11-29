eca717ab4e93d36435ff106928148949c91fc7b5
Aaron Petkau <aaron.petkau@gmail.com>
2020-11-28 23:50:09 -0600

    Skip over any file that is not a proper CARD:Live data file

--- card_live_dashboard/routes.py
+++ card_live_dashboard/routes.py
@@ -3,6 +3,7 @@ from os import listdir
 from pathlib import Path
 from io import BytesIO
 import zipfile
+import json
 
 import flask
 
@@ -27,10 +28,23 @@ def create_flask_routes(flask_app: flask.app.Flask, base_pathname: str, card_liv
     # Code derived from https://stackoverflow.com/a/27337047
     @flask_app.route(f'{base_pathname}/data/current')
     def download_data():
+        flask_app.logger.info(f'Request to download all data from [{card_live_data_dir}]')
         memory_file = BytesIO()
         with zipfile.ZipFile(memory_file, 'w', compression=zipfile.ZIP_STORED) as zf:
             files = [f for f in listdir(card_live_data_dir) if isfile(join(card_live_data_dir, f))]
             for file in files:
-                zf.write(join(card_live_data_dir, file), arcname=file)
+                file_path = join(card_live_data_dir, file)
+                with open(file_path) as f:
+                    valid_file = False
+                    try:
+                        json_obj = json.load(f)
+                        valid_file = True
+                    except Exception:
+                        pass
+                    if valid_file and 'rgi_main' in json_obj:
+                        zf.write(file_path, arcname=file)
+                    else:
+                        flask_app.logger.warning((f'File [{file_path}] is not a proper CARD:Live JSON file, '
+                                                 'skipping file in download request.'))
         memory_file.seek(0)
         return flask.send_file(memory_file, attachment_filename='card-live-data.zip', as_attachment=True)
